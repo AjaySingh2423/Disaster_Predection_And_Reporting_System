e9c8194577dc0c83457a64438ca67e26c72c7725
Ajay Singh <ajaysingh17242002@gmail.com>
2026-07-07 13:48:40 +0530

    Change style

--- frontend/src/data/map2.jsx
+++ frontend/src/data/map2.jsx
@@ -1,6 +1,20 @@
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import { useState, useEffect } from "react";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function MapPage() {
   const [position, setPosition] = useState(null);
